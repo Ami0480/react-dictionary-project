ee9ac6b828f912ee9541fb639767f66d6cd73426
Ami0480 <amifuku80@gmail.com>
2025-07-15 09:21:36 +0800

    Added serach engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <Dictionary />
+        <footer className="App-footer">
+          This dictionary was created by Ami Fukuyama
+        </footer>
+      </div>
     </div>
   );
 }
