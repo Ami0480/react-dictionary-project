288d1f0f48f86664ee82bdb260ca4d5b33c3fe02
Ami0480 <amifuku80@gmail.com>
2025-07-18 13:46:03 +0800

    Linked github and netlify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,27 @@ function App() {
       <div className="container">
         <Dictionary defaultKeyword="sunset" />
         <footer className="App-footer">
-          This dictionary was created by Ami Fukuyama
+          This dictionary was created by{" "}
+          <a href="https://github.com/Ami0480" target="_blank" rel="noreferrer">
+            Ami Fukuyama
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/Ami0480/react-dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            is sourced on GitHub
+          </a>
+          {"  "}and{" "}
+          <a
+            href="https://effortless-meringue-e38e27.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            is hosted on Netlify
+          </a>
+          .
         </footer>
       </div>
     </div>
