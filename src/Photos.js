29d8ae4bac0e2d9c36650fd1fe363a8790f060ed
Ami0480 <amifuku80@gmail.com>
2025-07-18 13:19:22 +0800

    removed keyword on photo

--- src/Photos.js
+++ src/Photos.js
@@ -11,11 +11,7 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    src={photo.src.tiny}
-                    className="image-fluid"
-                    alt={keyword}
-                  />
+                  <img src={photo.src.tiny} className="image-fluid" />
                 </a>
               </div>
             );
